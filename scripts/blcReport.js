e46e2ab57c4ee6eecbde24d11339c10f05305733
Kyle Taborski <ktabors@yahoo.com>
2023-04-18 21:38:40 -0700

    adding more runtime args and creating readme

--- scripts/blcReport.js
+++ scripts/blcReport.js
@@ -3,12 +3,21 @@ const {SiteChecker} = require('broken-link-checker');
 let options = {};
 let baseURL = '';
 let urlParamIndex = process.argv.indexOf('-u');
+let showExcluded = process.argv.indexOf('-e') !== -1;
+let showRedirects = process.argv.indexOf('-r') !== -1;
+let showTooManyRequests = process.argv.indexOf('-t') !== -1;
 let errorArray = [];
 let excludedArray = [];
 let redirectArray = [];
+let tooManyRequestsArray = []
 
 if (urlParamIndex !== -1 && urlParamIndex < process.argv.length - 1) {
   baseURL = process.argv[urlParamIndex + 1];
+}
+
+if (baseURL.length === 0) {
+  console.log('No base url defined.\nUsage: `yarn run -u <BASE_URL>`');
+  process.exit(1);
 }
 
 console.log(`Broken link scan of site using baseurl: \x1b[32m${baseURL}\x1b[0m`);
@@ -24,6 +33,11 @@ var siteChecker = new SiteChecker(options, {
         let redirectMessage = `\x1b[33mBroken (${result.brokenReason}):\x1b[0m \x1b[31m${result.url.redirected || result.url.resolved || result.url.original}\x1b[0m on page: ${result.base.resolved}`;
         if (redirectArray.indexOf(redirectMessage) === -1) {
           redirectArray.push(redirectMessage);
+        }
+      } else if (result.brokenReason === 'HTTP_429') {
+        let tooManyRequestsMessage = `\x1b[33mBroken (${result.brokenReason}):\x1b[0m \x1b[31m${result.url.redirected || result.url.resolved || result.url.original}\x1b[0m on page: ${result.base.resolved}`;
+        if (tooManyRequestsArray.indexOf(tooManyRequestsMessage) === -1) {
+          tooManyRequestsArray.push(tooManyRequestsMessage);
         }
       } else {//=> HTTP_404 and other errors
         let errorMessage = `\x1b[33mBroken (${result.brokenReason}):\x1b[0m \x1b[31m${result.url.redirected || result.url.resolved || result.url.original}\x1b[0m on page: ${result.base.resolved}`;
@@ -42,8 +56,21 @@ var siteChecker = new SiteChecker(options, {
     console.log(`Finished checking page: \x1b[32m${pageUrl}\x1b[0m`);
   },
   end: () => {
-    errorArray.forEach(error => console.log(error));
-    console.log(`\nErrors: ${errorArray.length}\nRedirects: ${redirectArray.length}\nExcluded: ${excludedArray.length}`);
+    console.log('Broken links:')
+    errorArray.forEach(error => console.log('  ', error));
+    if (showRedirects) {
+      console.log('Redirects:')
+      redirectArray.forEach(redirect => console.log('  ', redirect));
+    }
+    if (showTooManyRequests) {
+      console.log('Too many requests:')
+      tooManyRequestsArray.forEach(tooManyRequests => console.log('  ', tooManyRequests));
+    }
+    if (showExcluded) {
+      console.log('Excluded:')
+      excludedArray.forEach(excluded => console.log('  ', excluded));
+    }
+    console.log(`\nBroken links: ${errorArray.length}\nRedirects: ${redirectArray.length}\nToo many requests: ${tooManyRequestsArray.length}\nExcluded: ${excludedArray.length}`);
     console.log('\n\x1b[32mSite scan complete!\x1b[0m');
   }
 });
@@ -56,7 +83,6 @@ let killed = () => {
   console.log(`\nErrors: ${errorArray.length}\nRedirects: ${redirectArray.length}\nExcluded: ${excludedArray.length}`);
   console.log('\n\x1b[32mSite scan killed!\x1b[0m');
   process.exit();
-
 }
 
 // CTRL+C
